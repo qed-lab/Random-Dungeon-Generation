634c5a238047e26d470f73497ee5d4bec7bea88f
Aditya Suresh <ethanaditya@gmail.com>
2018-06-28 20:25:48 -0600

    First commit

---
+++ Assets/Scripts/CoordinatePair.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[System.Serializable]
+public struct CoordinatePair {
+	//defining b 
+	public int x, z;  
+	public CoordinatePair(int x, int z){          
+			this.x = x;
+			this.z = z;
+		}
+	//methods
+	//addition
+	public static CoordinatePair operator + (CoordinatePair  a, CoordinatePair  b) {
+		a.x += b.x;
+		a.z += b.z;
+		return a;
+	}
+	//subtraction
+	public static CoordinatePair operator - (CoordinatePair  a, CoordinatePair  b) {
+		a.x -= b.x;
+		a.z -= b.z;
+		return a;
+	}
+
+}
+      
---
+++ Assets/Scripts/Generate_Dungeon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_Dungeon : MonoBehaviour {
+	//reference vars
+	public GameObject ref_roomprefab;
+	//immutable vars
+	public int num_rooms;
+	public int rowsandcolumns;
+	public GameObject [] rooms;
+	// Use this for initialization
+	void Start () {
+		//initialization
+		rooms = new GameObject[num_rooms];
+		//generate n amount of rooms
+		rooms_generate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rooms_regenerate ();
+		}
+	}
+
+	void rooms_generate(){
+		for (int i = 0; i < num_rooms; i++) {
+			//instantiate room prefab
+			GameObject room = Instantiate (ref_roomprefab, this.transform.position, this.transform.rotation);
+			rooms [i] = room;
+			room.name = "Room " + (i + 1).ToString();    
+			       
+		}
+	}
+
+	void rooms_destroy(){
+		for (int i = 0; i < rooms.Length; i++) {
+			
+			Destroy (rooms [i].gameObject );
+			rooms [i] = null;
+		}
+	}
+
+	void rooms_regenerate(){
+		rooms_destroy ();
+		rooms_generate ();
+	}
+
+
+
+
+}

e7dbc69243a1ddd2c9440162742a4b6b45a9eb74
R. E. Cardona-Rivera <rogelio@cs.utah.edu>
2018-07-30 13:05:34 -0600

    Adding changes discussed during the meeting.

--- Assets/Scripts/Generate_Dungeon.cs
+++ Assets/Scripts/Generate_Dungeon.cs
@@ -2,64 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generate_Dungeon : MonoBehaviour {
-	//reference vars
-	public GameObject ref_roomprefab;
-	//immutable vars
-	public int num_rooms = 100;
-	public int rowsandcolumns;
-	public GameObject [] rooms;
-	int spawnAtX = 0, spawnAtZ = 0;
-	// Use this for initialization
-	void Start () {
-		//initialization
-		rooms = new GameObject[num_rooms];
-		transform.position = new Vector3 (spawnAtX, 0, spawnAtZ);
-		//generate n amount of rooms
-		rooms_generate();
-		rowsandcolumns = num_rooms;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.Space))
-		{
-			rooms_regenerate ();
-		}
-	}
+public class Generate_Dungeon : MonoBehaviour
+{
+    //reference vars
+    public GameObject ref_roomprefab;
+    //immutable vars
+    public int num_rooms = 100;
+    public int rowsandcolumns;
+    public GameObject[] rooms;
+    int spawnAtX = 0, spawnAtZ = 0;
+    // Use this for initialization
+    void Start()
+    {
+        //initialization
+        rooms = new GameObject[num_rooms];
+        transform.position = new Vector3(spawnAtX, 0, spawnAtZ);
 
-	void rooms_generate(){
-		for (int i = 0; i < num_rooms; i++) {
-			//instantiate room prefab
-			GameObject room = Instantiate (ref_roomprefab, transform.position, transform.rotation);
-			Room room_refScript = room.GetComponent<Room> ();
-			room_refScript.minX = spawnAtX;
-			room_refScript.minZ = spawnAtZ;
-			rooms [i] = room;
-			room.name = "Room " + (i + 1).ToString();    
+        //generate n amount of rooms
+        rooms_generate(num_rooms);
+        rowsandcolumns = num_rooms;
+    }
 
-			spawnAtX += 10;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rooms_regenerate();
+        }
+    }
 
-			if (spawnAtX == rowsandcolumns) {
-				spawnAtX = 0; 
-				spawnAtZ += 10;
-			}
+    void rooms_generate(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            //instantiate room prefab
+            GameObject room = Instantiate(ref_roomprefab, transform.position, transform.rotation);
+            Room room_refScript = room.GetComponent<Room>();
+            room_refScript.minX = spawnAtX;
+            room_refScript.minZ = spawnAtZ;
+            rooms[i] = room;
+            room.name = "Room " + (i + 1).ToString();
 
-		}
-	}
+            spawnAtX += 10;
 
-	void rooms_destroy(){
-		for (int i = 0; i < rooms.Length; i++) {
-			
-			Destroy (rooms [i].gameObject );
-			rooms [i] = null;
-		}
-	}
+            if (spawnAtX == rowsandcolumns)
+            {
+                spawnAtX = 0;
+                spawnAtZ += 10;
+            }
 
-	void rooms_regenerate(){
-		rooms_destroy ();
-		rooms_generate ();
-	}
+        }
+    }
+
+    void rooms_destroy()
+    {
+        for (int i = 0; i < rooms.Length; i++)
+        {
+
+            Destroy(rooms[i].gameObject);
+            rooms[i] = null;
+        }
+    }
+
+    void rooms_regenerate()
+    {
+        rooms_destroy();
+        rooms_generate();
+    }
 
 
 
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -8,7 +8,7 @@ public class Room : MonoBehaviour
     public int size;
     public int mod;
     public GameObject ref_cellprefab;
-	public int minX, minZ;
+    public int minX, minZ;
     public CoordinatePair[] dirs = {
 
         new CoordinatePair(1, 0),
@@ -17,21 +17,23 @@ public class Room : MonoBehaviour
         new CoordinatePair(0, -1)
 
     };
+
     //other vars    
-    public int cur_index;
+    public int currentIndex;
     public CoordinatePair cloneAt;
+
     //Use this for initialization
     public GameObject[,] CellsInRoom = new GameObject[10, 10];
-    public List<GameObject> acir = new List<GameObject>(); //abbreviated for active cells in room
+    public List<GameObject> activeCellsInRoom = new List<GameObject>(); //abbreviated for active cells in room
 
 
     void Start()
     {
-		transform.position = new Vector3 (minX, 0, minZ);
+        transform.position = new Vector3(minX, 0, minZ);
         GenerateSeed();
 
- 		print(cloneAt.x + " , " + cloneAt.z);
-		 
+        print(cloneAt.x + " , " + cloneAt.z);
+
         StartCoroutine(randomWalk());
     }
 
@@ -44,30 +46,31 @@ public class Room : MonoBehaviour
     void GenerateSeed()
     {
         //generating random coordinates to place seed
-        cloneAt = new CoordinatePair(Random.Range(minX, minX + 9), Random.Range(minZ, minZ + 9));
+        cloneAt = new CoordinatePair(Random.Range(minX, minX + 9), Random.Range(minZ, minZ + 9)); // sets up pointer to where to clone curnt cell
         //uses generatecell function
-        acir.Add(GenerateCell());
+        activeCellsInRoom.Add(GenerateCell());
     }
     private GameObject GenerateCell()
     {
 
         //references 
         GameObject ref_newcell = Instantiate(ref_cellprefab);
-        Ground_Cell ref_cellclass = ref_newcell.GetComponent<Ground_Cell>();
+        Ground_Cell groundCellScript = ref_newcell.GetComponent<Ground_Cell>();
+
         //putting reference inside the list
         CellsInRoom[cloneAt.x - minX, cloneAt.z - minZ] = ref_newcell;
+
         //initializing the object instance
         ref_newcell.name = "Cell at gridposition " + cloneAt.x + "," + cloneAt.z;
         ref_newcell.transform.position = new Vector3((float)cloneAt.x + 0.5f, 0f, (float)cloneAt.z + 0.5f);
         ref_newcell.transform.parent = transform;
-        ref_cellclass.coords = cloneAt;
+        groundCellScript.coords = cloneAt;
         return ref_newcell;
     }
 
     void GenerateStep()
     {
-
-        cur_index = acir.Count - 1;
+        currentIndex = activeCellsInRoom.Count - 1;
         mod = Random.Range(0, 4);
         cloneAt += dirs[mod];
 
@@ -76,24 +79,27 @@ public class Room : MonoBehaviour
 
         if (verifyBounds(cloneAt))
         {
-            acir.Add(GenerateCell());
+            activeCellsInRoom.Add(GenerateCell());
 
         }
     }
 
     // Change signature to accept inputs (float x, float z)
-	bool verifyBounds(CoordinatePair cur_cellPos)
+    bool verifyBounds(CoordinatePair cur_cellPos)
     {
-		if (CellsInRoom[cur_cellPos.x - minX, cur_cellPos.z - minZ] == null && cur_cellPos.x < minX + 10 && cur_cellPos.x > minX -1 && cur_cellPos.z < minZ + 10 && cur_cellPos.z > minZ - 1)
+        if (CellsInRoom[cur_cellPos.x - minX, cur_cellPos.z - minZ] == null && cur_cellPos.x < minX + 10 && cur_cellPos.x > minX - 1 && cur_cellPos.z < minZ + 10 && cur_cellPos.z > minZ - 1)
         {
             return true;
 
         }
         else
             return false;
-        acir.RemoveAt(cur_index);
-        cur_index = acir.Count - 1;
-		cur_cellPos = acir[cur_index].GetComponent<Ground_Cell>().coords;
+
+        // Everything below this line is "dead code":
+
+        activeCellsInRoom.RemoveAt(currentIndex);
+        currentIndex = activeCellsInRoom.Count - 1;
+        cur_cellPos = activeCellsInRoom[currentIndex].GetComponent<Ground_Cell>().coords;
     }
 
     IEnumerator randomWalk()

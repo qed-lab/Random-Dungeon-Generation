442e822d5517f4e1dba9ba886c638d54cd25ff2b
Aditya Suresh <ethanaditya@gmail.com>
2018-08-03 12:34:27 -0600

    tweaked romm generation logic to not spit errors and cell frequency greatly increased

--- Assets/Scripts/CoordinatePair.cs
+++ Assets/Scripts/CoordinatePair.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
 public struct CoordinatePair {
-	//defining b 
-	public int x, z;  
+	//defining coordinates
+	public int x, z; 
+
+	//constructor
 	public CoordinatePair(int x, int z){          
 			this.x = x;
 			this.z = z;
 		}
 	//methods
+
 	//addition
 	public static CoordinatePair operator + (CoordinatePair  a, CoordinatePair  b) {
 		a.x += b.x;
 		a.z += b.z;
 		return a;
 	}
+
 	//subtraction
 	public static CoordinatePair operator - (CoordinatePair  a, CoordinatePair  b) {
 		a.x -= b.x;
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,56 +5,51 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
     //ref vars
-    public int size;
+	public GameObject refCellPrefab;
+
+	//room-generation oriented vars
     public int mod;
-    public GameObject ref_cellprefab;
-    public int minX, minZ;
-    public CoordinatePair[] dirs = {
-
-        new CoordinatePair(1, 0),
-        new CoordinatePair(-1, 0),
-        new CoordinatePair(0, 1),
-        new CoordinatePair(0, -1)
-
-    };
-
-    //other vars    
-    public int currentIndex;
-    public CoordinatePair cloneAt;
-
-    //Use this for initialization
+	public int minX, minZ; //publicly set minimum x and z axis coordinates for random seed and boundary generation
+	public List<CoordinatePair> UnreadDirs; //stores directions to check
+	public int currentIndex;//current cell to look at in list of active cells(cells which are active have directions available to them)
+	public CoordinatePair cloneAt;//where to clone the current cell   
+  
+    //storage
     public GameObject[,] CellsInRoom = new GameObject[10, 10];
     public List<GameObject> activeCellsInRoom = new List<GameObject>(); //abbreviated for active cells in room
 
 
     void Start()
     {
+		//initialize list
+		UnreadDirs = new List<CoordinatePair> ();
+
+		//initialize room position
         transform.position = new Vector3(minX, 0, minZ);
+
+		//generate the starting cell
         GenerateSeed();
 
-        print(cloneAt.x + " , " + cloneAt.z);
-
+        //run the coroutine for full room generation
         StartCoroutine(randomWalk());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     //generates the seed to start the room
     void GenerateSeed()
     {
         //generating random coordinates to place seed
         cloneAt = new CoordinatePair(Random.Range(minX, minX + 9), Random.Range(minZ, minZ + 9)); // sets up pointer to where to clone curnt cell
+
         //uses generatecell function
         activeCellsInRoom.Add(GenerateCell());
     }
+
+	//generates a cell with relevant information
     private GameObject GenerateCell()
     {
 
         //references 
-        GameObject ref_newcell = Instantiate(ref_cellprefab);
+        GameObject ref_newcell = Instantiate(refCellPrefab);
         Ground_Cell groundCellScript = ref_newcell.GetComponent<Ground_Cell>();
 
         //putting reference inside the list
@@ -65,41 +60,104 @@ public class Room : MonoBehaviour
         ref_newcell.transform.position = new Vector3((float)cloneAt.x + 0.5f, 0f, (float)cloneAt.z + 0.5f);
         ref_newcell.transform.parent = transform;
         groundCellScript.coords = cloneAt;
-        return ref_newcell;
-    }
 
+		//returns the object instance
+        return ref_newcell;
+    }  
+
+	//explores dirs and adds cells
     void GenerateStep()
     {
-        currentIndex = activeCellsInRoom.Count - 1;
-        mod = Random.Range(0, 4);
-        cloneAt += dirs[mod];
+		//init dirs to check
+		UnreadDirs.Clear();
+		UnreadDirs.Add( new CoordinatePair(1, 0));//right
+		UnreadDirs.Add(	new CoordinatePair(-1, 0));//left
+		UnreadDirs.Add(	new CoordinatePair(0, 1));//up
+		UnreadDirs.Add(	new CoordinatePair(0, -1));//down
 
-        cloneAt.x = Mathf.Abs(cloneAt.x); //prevents runtime error 
+		//sets current index to last item in acir
+		currentIndex = activeCellsInRoom.Count - 1;
+		if (currentIndex < 0) {
+			currentIndex = 0;
+		}
+		//sets mod to which index of UnreadDirs to look at
+		mod = Random.Range(0, 4);
+
+		//modifies cloneat based on mod
+        cloneAt += UnreadDirs [mod];  
+
+		//prevents runtime error 
+        cloneAt.x = Mathf.Abs(cloneAt.x); 
         cloneAt.z = Mathf.Abs(cloneAt.z);
 
-        if (verifyBounds(cloneAt))
-        {
-            activeCellsInRoom.Add(GenerateCell());
+		//while I still have directions to check
+		while (!(UnreadDirs.Count == 0)) 
+		{
+			//if my direction to check (unreadDirs[mod] is invalid
+			if (!VerifyBounds (cloneAt)) {
+				
+				//undo the direction's modifiers
+				cloneAt -= UnreadDirs [mod];
 
-        }
+				//remove the direction from the list
+				UnreadDirs.RemoveAt (mod); 
+
+				//pick a new random direction from the list of available dirs
+				mod = Random.Range (0, UnreadDirs.Count);
+
+				//prevents runtime error (again)
+				cloneAt.x = Mathf.Abs(cloneAt.x); 
+				cloneAt.z = Mathf.Abs(cloneAt.z);
+
+			} else {
+				
+				//else if dir is valid then exit loop
+				break;
+			}
+		}
+
+		//if I exited the loop because I found a valid direction
+		if (!(UnreadDirs.Count == 0)) {
+			//add the cell in that direction
+			activeCellsInRoom.Add (GenerateCell ());
+			//reset current index to new length (index wise)
+			currentIndex = activeCellsInRoom.Count - 1; 
+		} else {
+			//I exited the loop because I have no directions left to check
+			print (currentIndex + "," + activeCellsInRoom.Count);
+			if (!(activeCellsInRoom.Count == 0)) {
+				activeCellsInRoom.RemoveAt(currentIndex);
+			}
+
+			currentIndex -= 1;  
+			if (currentIndex < 0) {
+				currentIndex = 0;
+			}
+			if (!(activeCellsInRoom.Count == 0)) {
+				cloneAt = activeCellsInRoom [currentIndex].GetComponent<Ground_Cell> ().coords;
+			}
+		}
     }
 
     // Change signature to accept inputs (float x, float z)
-    bool verifyBounds(CoordinatePair cur_cellPos)
+    bool VerifyBounds(CoordinatePair cur_cellPos)
     {
-        if (CellsInRoom[cur_cellPos.x - minX, cur_cellPos.z - minZ] == null && cur_cellPos.x < minX + 10 && cur_cellPos.x > minX - 1 && cur_cellPos.z < minZ + 10 && cur_cellPos.z > minZ - 1)
-        {
-            return true;
 
+		if (cur_cellPos.x < minX + 10 && cur_cellPos.x > minX - 1 && cur_cellPos.z < minZ + 10 && cur_cellPos.z > minZ - 1)
+        {
+			if(CellsInRoom[cur_cellPos.x - minX,  cur_cellPos.z - minZ] == null){
+            	return true;
+				 
+			}
+			else
+				return false;
         }
         else
             return false;
+		
+        
 
-        // Everything below this line is "dead code":
-
-        activeCellsInRoom.RemoveAt(currentIndex);
-        currentIndex = activeCellsInRoom.Count - 1;
-        cur_cellPos = activeCellsInRoom[currentIndex].GetComponent<Ground_Cell>().coords;
+        
     }
 
     IEnumerator randomWalk()
